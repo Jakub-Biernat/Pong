c7ec733d666e4c36812be0de965bb2a7aacbb94b
Jakub-Biernat <k.biernat@vp.pl>
2024-03-24 22:18:56 +0100

    screen setup

---
+++ main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+WIDTH = 800
+HEIGHT = 600
+
+screen = Screen()
+screen.setup(width=WIDTH, height=HEIGHT)
+screen.bgcolor("black")
+screen.title("Pong")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
