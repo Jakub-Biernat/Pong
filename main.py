3784284644dc1cba8d54bec8fbb91c178d45deb8
Jakub-Biernat <k.biernat@vp.pl>
2024-03-24 22:48:39 +0100

    paddle OOP

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
 
 WIDTH = 800
 HEIGHT = 600
-
-
-def up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+L_POSITION = (-350, 0)
+R_POSITION = (350, 0)
 
 
 screen = Screen()
@@ -20,17 +13,13 @@ screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
+l_paddle = Paddle(position=L_POSITION)
+r_paddle = Paddle(position=R_POSITION)
+
 screen.listen()
-screen.onkeypress(up, "w")
-screen.onkeypress(down, "s")
+screen.onkeypress(l_paddle.up, "w")
+screen.onkeypress(l_paddle.down, "s")
 
-
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.setposition(x=350, y=0)
 
 is_running = True
 while is_running:
